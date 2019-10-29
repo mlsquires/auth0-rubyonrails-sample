b4278a6450cb0508e6c694918334a52a002e9131
Michael Lee Squires <msquires@navigatingcancer.com>
2019-10-29 16:06:06 +0000

    misc config

---
+++ 01-Login/probe.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x -v
+
+BASE_URL=http://localhost:3000
+
+curl --silent $BASE_URL/ > ,root
+curl --silent $BASE_URL/logout > ,logout
+curl --silent $BASE_URL/dashboard/show > ,dashboard_show
+curl --silent $BASE_URL/auth/auth0/callback > ,auth_auth0_callback
+curl --silent $BASE_URL/auth/failure > ,auth_failure
+
+set +x +v
