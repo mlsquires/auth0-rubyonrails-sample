f677d53fd00a779d06c516fd416ef12cca38115c
Michael Lee Squires <michael_lee_squires@pobox.com>
2020-02-21 09:31:26 -0800

    dump jwt

--- 03-User-Profile/app/controllers/auth0_controller.rb
+++ 03-User-Profile/app/controllers/auth0_controller.rb
@@ -1,10 +1,17 @@
+require 'json'
+require 'jwt'
+
 class Auth0Controller < ApplicationController
   def callback
     # OmniAuth places the User Profile information (retrieved by omniauth-auth0) in request.env['omniauth.auth'].
     # In this tutorial, you will store that info in the session, under 'userinfo'.
     # If the id_token is needed, you can get it from session[:userinfo]['credentials']['id_token'].
     # Refer to https://github.com/auth0/omniauth-auth0#auth-hash for complete information on 'omniauth.auth' contents.
-    session[:userinfo] = request.env['omniauth.auth']
+    userinfo = JSON.load(request.env['omniauth.auth'])
+    id_token = userinfo['credentials']['id_token']
+    userinfo[:jwt] = JWT.decode(id_token,nil,false)
+
+    session[:userinfo] = JSON.pretty_generate(userinfo)
 
     redirect_to '/dashboard'
   end
@@ -14,4 +21,12 @@ class Auth0Controller < ApplicationController
   def failure
     @error_msg = request.params['message']
   end
+end
+
+def dump_jwt(jwt)
+  # Set password to nil and validation to false otherwise this won't work
+  decoded = JWT.decode jwt, nil, false
+  Rails.logger.info %Q{auth0: #{self}.dump_jwt: decoded: #{decoded.awesome_inspect}}
+rescue JWT::DecodeError
+  Rails.logger.info %Q{auth0: #{self}.dump_jwt: received invalid jwt}
 end
